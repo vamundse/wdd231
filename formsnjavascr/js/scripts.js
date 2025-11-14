592950d93bb21158935a35bce08f914edb8a96ed
vamundse <vamundse@gmail.com>
2025-11-14 08:58:31 +0100

    forms activity completed

---
+++ formsnjavascr/js/scripts.js
@@ -0,0 +1,19 @@
+const myInfo = new URLSearchParams(window.location.search);
+console.log(myInfo);
+
+/*
+console.log(myInfo.get('first'));
+console.log(myInfo.get('last'));
+console.log(myInfo.get('phone'));
+console.log(myInfo.get('email'));
+console.log(myInfo.get('ordinance'));
+console.log(myInfo.get('date'));
+console.log(myInfo.get('location'));
+*/
+
+document.querySelector('#results').innerHTML = `
+<p>Appointment for ${myInfo.get('first')} ${myInfo.get('last')}</p>
+<p>Proxy ${myInfo.get('ordinance')} on ${myInfo.get('date')} in the ${myInfo.get('location')} temple</p>
+<p>Your Phone: ${myInfo.get('phone')}</p>
+<p>Your Email: ${myInfo.get('email')}</p>
+`;
